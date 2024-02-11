b2ef5b471ed0df5942c800d3502b726259d40c65
IceMoonMagic <icemoonmagic@gmail.com>
2024-02-11 12:51:47 -0600

    Added an encrypted string type
    Usage: <attr name>: Mapped[str] = mapped_column(type_=EncryptedStr,...)
    I see no easy way to make it generic without sqlalchemy giving more params.
    I was unable to find a good way to let each cog use its own SecretBox.

--- database.py
+++ database.py
@@ -13,6 +13,7 @@ import nacl.secret
 from sqlalchemy import (
     BinaryExpression,
     DateTime,
+    LargeBinary,
     TypeDecorator,
     delete,
     inspect,
@@ -96,7 +97,20 @@ logger = get_logger(__name__)
 DB_FILE = r"saves/database.db"
 TEMP_FILE = None
 
+BOX = nacl.secret.SecretBox(b"Change Hard Coded Encryption Key")
+
 S = TypeVar("S", bound="Storable")
+
+
+class EncryptedStr(TypeDecorator):
+    impl = LargeBinary
+    cache_ok = True
+
+    def process_bind_param(self, value: str, dialect) -> bytes:
+        return BOX.encrypt(value.encode())
+
+    def process_result_value(self, value: bytes, dialect) -> str:
+        return BOX.decrypt(value).decode()
 
 
 # https://docs.sqlalchemy.org/en/14/core/custom_types.html#store-timezone-aware-timestamps-as-timezone-naive-utc
@@ -115,70 +129,24 @@ class TZDateTime(TypeDecorator):
         return value
 
 
-ENGINE = create_async_engine("sqlite://")
+ENGINE = create_async_engine("sqlite+aiosqlite://")
 
 
 class Storable(DeclarativeBase):
     temp: bool = False
 
-    encrypt_attrs: list = []
-    box: nacl.secret.SecretBox | None = None
-
     async def save(self: S):
-        # if not self.is_ready():
-        #     raise MissingEncryptionKey(self.__class__)
-        #
-        # encrypted = {}
-        # unique_chars: int = len(str(len(self.encrypt_attrs)))
-        # primary_value = f"{getattr(self, self.primary_key_name)}"
-        # nonce_base = (
-        #     f"{primary_value[:24 - unique_chars]:0{24 - unique_chars}}"
-        # )
-        # for i, attr in enumerate(self.encrypt_attrs):
-        #     nonce = f"{i:0{unique_chars}}{nonce_base}".encode()
-        #
-        #     raw_value: str = getattr(self, attr)
-        #     bytes_value = raw_value.encode()
-        #     encrypted_value = self.box.encrypt(bytes_value, nonce).ciphertext
-        #     encrypted[attr] = encrypted_value.hex()
-
         async with AsyncSession(ENGINE) as session:
             session.add(self)
             await session.commit()
 
-    def decrypt(self: S, decrypt_inplace: bool = True) -> S:
-        work_on: S = self if decrypt_inplace else self.copy()
-
-        if not work_on.is_ready():
-            raise MissingEncryptionKey(self.__class__)
-
-        unique_chars: int = len(str(len(work_on.encrypt_attrs)))
-        primary_value = f"{getattr(work_on, work_on.primary_key_name)}"
-        nonce_base = (
-            f"{primary_value[:24 - unique_chars]:0{24 - unique_chars}}"
-        )
-        for i, attr in enumerate(work_on.encrypt_attrs):
-            nonce = f"{i:0{unique_chars}}{nonce_base}".encode()
-
-            hex_value: str = getattr(work_on, attr)
-            bytes_value = bytes.fromhex(hex_value)
-            decrypted_value = work_on.box.decrypt(bytes_value, nonce)
-            # decrypted[attr]: str = decrypted_value.decode()
-            setattr(work_on, attr, decrypted_value.decode())
-
-        return work_on
-
     @classmethod
-    async def load(
-        cls: type[S], primary_key, decrypt: bool = True
-    ) -> S | None:
+    async def load(cls: type[S], primary_key) -> S | None:
         async with AsyncSession(ENGINE) as session:
             return await session.get(cls, primary_key)
 
     @classmethod
-    async def load_all(
-        cls: type[S], decrypt: bool = True, *where: BinaryExpression
-    ) -> list[S]:
+    async def load_all(cls: type[S], *where: BinaryExpression) -> list[S]:
         async with AsyncSession(ENGINE) as session:
             stmt = select(cls)
             for w in where:
@@ -192,28 +160,12 @@ class Storable(DeclarativeBase):
             stmt = delete(cls).where(pk == primary_key)
             await session.execute(stmt)
 
-    @classmethod
-    def is_ready(cls) -> bool:
-        return len(cls.encrypt_attrs) == 0 or cls.box is not None
-
-    @classmethod
-    def set_key(cls, key: str | bytes):
-        if isinstance(key, str):
-            key = key.encode()
-        cls.box = nacl.secret.SecretBox(key)
-
-    @classmethod
-    def clear_key(cls):
-        cls.box = None
-
 
 class MissingEncryptionKey(RuntimeError):
-    def __init__(self, type_: type[Storable] | Storable, *args):
-        type_ = type_.__class__ if isinstance(type_, Storable) else type_
+    def __init__(self, *args):
         if len(args) == 0:
-            args = (f"Missing encryption key for {type_}",)
+            args = (f"Missing encryption key",)
         super().__init__(*args)
-        self.storable: Storable = type_
 
 
 async def does_table_exist(table_name, temp: bool) -> bool:
